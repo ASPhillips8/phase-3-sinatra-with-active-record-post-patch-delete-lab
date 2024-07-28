db8ce3fb62a8637d52f4c97f3ed0714fab8e1482
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-28 16:39:09 -0400

    Define Post method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
-  
+
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json
   end
-  
+
   get '/bakeries/:id' do
     bakery = Bakery.find(params[:id])
     bakery.to_json(include: :baked_goods)
@@ -20,6 +20,15 @@ class ApplicationController < Sinatra::Base
   get '/baked_goods/most_expensive' do
     # see the BakedGood class for the  method definition of `.by_price`
     baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
+
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
     baked_good.to_json
   end
 
